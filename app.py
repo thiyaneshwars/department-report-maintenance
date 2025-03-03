21e496f41a42209b8a0848865242b6b896e1bf36
Thiyaneshwar S <138683953+thiyaneshwars@users.noreply.github.com>
2025-03-03 12:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ import streamlit as st
 import requests
 
 # Flask API URL (Change if deployed)
-API_URL = "https://87ee-36-255-17-205.ngrok-free.app"
+API_URL = " https://eb15-36-255-17-205.ngrok-free.app"
 
 # Streamlit UI
 st.title("📑 Department Report Maintenance")
 
 # Input fields for report submission
 st.subheader("Submit a New Report")
-department = st.text_input("Department")
+Domain = st.text_input("Domain")
 member = st.text_input("Member Name")
 task = st.text_area("Task Description")
 
 if st.button("Submit Report"):
-    if department and member and task:
+    if Domain and member and task:
         response = requests.post(f"{API_URL}/add_report", json={
-            "department": department,
+            "Domain": Domain,
             "member": member,
             "task": task
         })
