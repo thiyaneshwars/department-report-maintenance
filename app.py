a43895236306ebdc5511b6c526735f099efa84e4
Thiyaneshwar S <138683953+thiyaneshwars@users.noreply.github.com>
2025-03-05 14:24:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,23 +10,35 @@ st.title("📑IITM Project Report Maintenance")
 
 # Input fields for report submission
 st.subheader("Submit a New Report")
-department = st.text_input("Project Name")
-member = st.text_input("Member Name")
+
+# Department selection with dropdown
+departments = ["HUMAN FACE RECOGNITION AND MOVEMENT RECOGNITION","TELUGU DATA AUGMENTATION","BERT FINETUNING","ENGLISH AUGMENTATION","T5 FINETUNING","ASR TELUGU"]  # Example departments
+selected_department = st.selectbox("Project Domain", departments)
+
+# Member selection based on department
+members = {
+    "HUMAN FACE RECOGNITION AND MOVEMENT RECOGNITION": ["VARATHARAJAN","SURENDAR KRISHANA","RUBAN B","KAVINESH K","ARUNHARIHARAN","PAVITHRAN G","PRAVEEN S"],
+    "TELUGU DATA AUGMENTATION": ["MADHAN J","ABINITHI G","MATHIARASI E","CHARAN B","SRI VISHNU TS"],
+    "BERT FINETUNING": ["THRISHA S","MUTHU DEEPAK V","DHARANISRI"],
+    "ENGLISH AUGMENTATION": ["ANUSH S","KIRITHIKA G","NANDHAKUMAR R","SUSEE KARTHIKA A","DINESH S","EZHILANAND V","SUBASREE R","VARUN A K"],
+    "T5 FINETUNING":["KAVIN R" ,"KARMUGILAN G R"],
+    "ASR TELUGU":["VASANTH S" ,"KAVIN R","MADHAN J","GOWTHAM S"]
+    
+}
+
+selected_member = st.selectbox("Member Name", members.get(selected_department, []))
+
 task = st.text_area("Task Description")
 
 if st.button("Submit Report"):
-    if department and member and task:
+    if selected_department and selected_member and task:
         response = requests.post(f"{API_URL}/add_report", json={
-            "department": department,
-            "member": member,
+            "department": selected_department,
+            "member": selected_member,
             "task": task
         })
         st.success(response.json().get("message"))
     else:
         st.warning("Please fill all fields!")
-
-
-
-
 
 
